130311aa8a42ff902702dd4e11ed986941fd94bd
pqndamic <165428839+pqndamic@users.noreply.github.com>
2024-10-07 11:28:07 -0700

    health pack stuff
    health pack assets

---
+++ Assets/Scripts/HealthPackSpawn.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HealthPackSpawner : MonoBehaviour
+{
+    public GameObject HealthPack;  // The prefab for the health pack
+    public int numberOfHealthPacks = 10;  // Number of health packs to spawn
+    public float spawnRadius = 50f;      // Radius in which to spawn the health packs
+    public LayerMask groundLayer;        // Layer for ground if needed to filter collisions
+
+    void Awake()
+    {
+        SpawnHealthPacks();
+    }
+
+    void SpawnHealthPacks()
+    {
+        for (int i = 0; i < numberOfHealthPacks; i++)
+        {
+            Vector3 randomPosition = GetRandomNavMeshPosition();
+            if (randomPosition != Vector3.zero)
+            {
+                Instantiate(HealthPack, randomPosition, Quaternion.identity);
+            }
+        }
+    }
+
+    Vector3 GetRandomNavMeshPosition()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * spawnRadius;
+        randomDirection += transform.position;  // Offset from the center of the map
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomDirection, out hit, spawnRadius, NavMesh.AllAreas))
+        {
+            // The hit position is valid on the nav mesh
+            return hit.position;
+        }
+
+        // If it couldn't find a position on the NavMesh, return zero and try again
+        return Vector3.zero;
+    }
+
+}
