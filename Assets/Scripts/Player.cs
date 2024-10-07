add8b9b64b9f4fb5517937e4566c16512adf20d8
Jason Jiang <jasontomjiang@gmail.com>
2024-10-06 19:29:32 -0700

    Added scene to build
    Fixes issue

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
-
 using UnityEngine;
+
+using UnityEngine.SceneManagement;
 using UnityEngine.UI; // Make sure to include this for using UI components
 
 public class Player : Character
