4e76a317db68c47bfff3790a61a336ffeda17145
hiprincetonliu <hiprincetonliu@gmail.com>
2024-10-06 19:33:08 -0700

    Enemies now follow player and can't move through walls

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,7 +11,9 @@ public class Enemy : Character
     public float followSpeed = 0.5f;    // Speed at which the enemy follows the player
     public float attackRange = 1.5f;  // Distance at which the enemy stops following and starts attacking
 
+    [SerializeField] Transform target;
 
+    UnityEngine.AI.NavMeshAgent agent;
     
     protected override void Start()
     {
@@ -22,6 +24,9 @@ public class Enemy : Character
             return; 
         }
         InvokeRepeating("RepeatAttack", 1f, attackEvery);
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     void RepeatAttack() {
@@ -37,16 +42,17 @@ public class Enemy : Character
     void Update()
     {
         // Move the enemy towards the player if the player is not in attack range
-        float distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
-        if (distanceToPlayer > attackRange)
+        // float distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
+        agent.SetDestination(target.position);
+        // if (distanceToPlayer > attackRange)
         {
-            // Face towards the player (rotate only on the Z axis)
-            Vector2 direction = (playerTransform.position - transform.position).normalized;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, 0, angle - 90); // Rotate on Z-axis
+            // // Face towards the player (rotate only on the Z axis)
+            // Vector2 direction = (playerTransform.position - transform.position).normalized;
+            // float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            // transform.rotation = Quaternion.Euler(0, 0, angle - 90); // Rotate on Z-axis
 
-            // Move towards the player
-            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, followSpeed * Time.deltaTime);
+            // // Move towards the player
+            // transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, followSpeed * Time.deltaTime);
         }
     }
 }
