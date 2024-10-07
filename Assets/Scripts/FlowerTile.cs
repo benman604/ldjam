b5a51320221c899893cd33ccbb89684d0cda14bc
Jason Jiang <jasontomjiang@gmail.com>
2024-10-07 01:02:52 -0700

    FlowerTile Script

--- Assets/Scripts/FlowerTile.cs
+++ Assets/Scripts/FlowerTile.cs
@@ -1,14 +1,13 @@
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
-[CreateAssetMenu(fileName = "NewFlowerTile", menuName = "Tiles/FlowerTile")]
-public class FlowerTile : Tile
+public class Flower : MonoBehaviour
 {
-    public void OnPlayerEnter(Player player)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        // Logic for when the player interacts with the flower tile
-        player.IncreaseFlowerCount();
-        // Optionally, you can set this tile to null to remove it
-        // or replace it with another tile.
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
