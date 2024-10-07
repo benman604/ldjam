03cafd4db0a303c3113a1b79b8eb6efa096f7d7c
Jason Jiang <jasontomjiang@gmail.com>
2024-10-06 18:28:20 -0700

    Stamina Bar reimplemented

--- Assets/Scripts/StaminaBar.cs
+++ Assets/Scripts/StaminaBar.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Include for using UI components
 
 public class StaminaBar : MonoBehaviour
 {
     public float stamina = 100f;
     public float maxStamina = 100f;
-    SpriteRenderer spriteRenderer;
-    float initialWidth;
+    
+    private RectTransform rectTransform; // Reference to the RectTransform
+    private float initialWidth;
 
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();    
-        initialWidth = spriteRenderer.transform.localScale.x;
+        rectTransform = GetComponent<RectTransform>(); // Get the RectTransform component
+        initialWidth = rectTransform.sizeDelta.x; // Store the initial width of the stamina bar
     }
 
     void Update()
     {
-        spriteRenderer.transform.localScale = new Vector3(initialWidth * stamina / maxStamina, spriteRenderer.transform.localScale.y, spriteRenderer.transform.localScale.z);
+        // Calculate the new width based on current stamina
+        float newWidth = initialWidth * (stamina / maxStamina);
+        
+        // Update the size of the RectTransform to reflect the new width
+        rectTransform.sizeDelta = new Vector2(newWidth, rectTransform.sizeDelta.y); // Update width, keep height unchanged
+
+    }
+
+    public void SetStamina(float newStamina)
+    {
+        stamina = Mathf.Clamp(newStamina, 0f, maxStamina); // Ensure stamina is within limits
     }
 }
